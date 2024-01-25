875d8934cabc897de1a32ca89280a6f74373844d
klvnnn <klvnnnyatiga@gmail.com>
2024-01-25 21:10:39 +0700

    [KELVIN] Test Commit

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>PPDB - SMAN 2 PASAMAN</title>
+    <title>PPDB - SMAN 2 Kelvin</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
