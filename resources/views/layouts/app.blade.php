bf3c2766f2e70fb89f4b28cb36140bb3edfba330
klvnnn <klvnnnyatiga@gmail.com>
2024-01-26 00:36:50 +0700

    [UPDATE] Fixx dashboard copyright

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -307,7 +307,7 @@
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="copyright">
-            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>PPDB Online</span></strong>. All Rights Reserved
         </div>
     </footer><!-- End Footer -->
 
